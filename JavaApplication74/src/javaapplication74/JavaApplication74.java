34d57df1bf254d8913bae076f584325dfd80b571
Lidze <30416802+Lidze@users.noreply.github.com>
2018-06-22 10:30:11 +0300

    Update JavaApplication74.java

--- JavaApplication74/src/javaapplication74/JavaApplication74.java
+++ JavaApplication74/src/javaapplication74/JavaApplication74.java
@@ -6,3 +6,19 @@ public class WiniumDemo {
         WebDriver driver = new RemoteWebDriver(new URL("http://localhost:9999"),cap);
 
         WebElement window = driver.findElement(By.className("тут класс нейм программы"));
+
+        
+       https://info.ranorex.com/e/428272/tudio-downloadlinkurl-type-exe/b9sw7c/885661541/400f88f075f3e937d868aaed8f85972d5b2ca2ae7f31e
+        
+        
+        
+       
+https://info.ranorex.com/e/428272/email-utm-campaign-en-webinars/b823fc/885661541
+
+https://info.ranorex.com/e/428272/il-utm-campaign-en-screencasts/9p91sp/885661541
+
+https://info.ranorex.com/e/428272/ail-utm-campaign-en-user-guide/b9sw7p/885661541
+
+https://info.ranorex.com/e/428272/dium-email-utm-campaign-en-faq/9p91st/885661541
+
+https://info.ranorex.com/e/428272/um-email-utm-campaign-en-forum/9p91sw/885661541
